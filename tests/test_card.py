538cc389276a15370e19463672f451e0bc4c2a80
Yasir Khan <yasir.khan@zoopla.co.uk>
2022-01-21 10:18:43 +0000

    add test for wrong rank

--- tests/test_card.py
+++ tests/test_card.py
@@ -10,8 +10,16 @@ class TestCard(unittest.TestCase):
 
     def test_invalid_suit_raises_exception(self):
         with self.assertRaises(Exception) as context:
-            wrong_suit = Card("Club", "Ace")
-        self.assertEqual("Suit must be one of (♠, ♥, ♣, ♦)",  str(context.exception))
+            Card("Club", "Ace")
+        self.assertEqual("Suit must be one of (♠, ♥, ♣, ♦)", str(context.exception))
+
+    def test_invalid_suit_raises_exception(self):
+        with self.assertRaises(Exception) as context:
+            Card("♠", "Wrong")
+        self.assertEqual(
+            "Rank must be one of (2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A)",
+            str(context.exception),
+        )
 
 
 if __name__ == "main":
