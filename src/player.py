08b6373e7f689e70f31506a446439cdeab4ad547
Yasir Khan <yasir.khan@zoopla.co.uk>
2022-02-11 09:37:46 +0000

    add ability to add multiple cards

--- src/player.py
+++ src/player.py
@@ -15,7 +15,10 @@ class Player:
         return self.all_cards.pop(0)
 
     def add_cards(self, new_cards):
-        self.all_cards.append(new_cards)
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)    
 
     def __str__(self):
         return f"Player {self.name} has {len(self.all_cards)} cards"
--- tests/test_player.py
+++ tests/test_player.py
@@ -8,12 +8,18 @@ class TestDeck(unittest.TestCase):
         self.assertEqual(player_one.name, "Dave")
         self.assertEqual(len(player_one.all_cards), 0)
 
-    def test_add_cards(self):
+    def test_add_one_card(self):
         player_one = Player("Dave")
         self.assertEqual(len(player_one.all_cards), 0)
         player_one.add_cards("3♥")
         self.assertEqual(len(player_one.all_cards), 1)
         self.assertEqual(player_one.all_cards[0], "3♥")
+
+    def test_add_list_of_cards(self):   
+        player_one = Player("Dave")
+        self.assertEqual(len(player_one.all_cards), 0)
+        player_one.add_cards(["3♥", "2♠", "4♥", "3♠"])
+        self.assertEqual(len(player_one.all_cards), 4)
 
     def test_remove_one_card(self):
         player_one = Player("Dave")
