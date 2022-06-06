f1982fbc7f78eafb3857e9088fc6bb76fa89e826
Yasir Khan <yasir.khan@zoopla.co.uk>
2022-06-06 09:41:16 +0100

    update logic to continue playing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 
 
 def countdown(t):
-    sleep = time.sleep(1)
+
     while t > 0:
         print(f"GAME STARTING IN {t} ⏰ ")
         t -= 1
@@ -12,13 +12,21 @@ def countdown(t):
 
 if __name__ == "__main__":
 
-    player_one = input("Please enter the name for Player One: ")
-    player_two = input("Please enter the name for Player Two: ")
-    countdown(3)
-    print("GAME STARTING 🚀🚀🚀")
-    try:
-        play_game(player_one, player_two)
-    except Exception as e:
-        print(f"Print something went wrong due to {e}")
-    else:
-        print(f"GAME OVER 🏁 Thanks for playing {player_one} & {player_two} 🤗")
+    play_on = True
+
+    while play_on:
+
+        player_one = input("Please enter the name for Player One: ")
+        player_two = input("Please enter the name for Player Two: ")
+        countdown(3)
+        print("GAME STARTING 🚀🚀🚀")
+        try:
+            play_game(player_one, player_two)
+        except Exception as e:
+            print(f"Print something went wrong due to {e}")
+        else:
+            response = input("Would you like to continue? (y/n): ")
+
+            if response == "n":
+                print(f"GAME OVER 🏁 Thanks for playing {player_one} & {player_two} 🤗")
+                play_on = False
